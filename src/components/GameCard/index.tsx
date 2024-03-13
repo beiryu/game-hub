2b759f88444261572a5f85b6d559d2bd9484ecef
Beiryu <dinhnguyenkhanh210401@gmail.com>
2024-03-13 21:38:53 +0700

    feat: add game heading and emoji

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -1,7 +1,6 @@
-import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
-import React from "react";
+import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
 import CriticScore from "../CriticScore";
-import Emoji from "./Emoji";
+import Emoji from "../Emoji";
 import PlatformIconList from "../PlatformIconList";
 import { Game } from "../../hooks/useGames";
 import getCroppedImageUrl from "../../services/image-url";
