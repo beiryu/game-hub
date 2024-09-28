8607426c8471b17beba3718ee681e922a98d3581
Beiryu <dinhnguyenkhanh210401@gmail.com>
2024-09-28 16:37:22 +0700

    feat: build expandable text

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,15 +1,11 @@
 import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
 import { Heading, Spinner, Text } from "@chakra-ui/react";
+import ExpandableText from "../components/ExpandableText";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
-
-  console.log(slug);
-
   const { data: game, isLoading, error } = useGame(slug!);
-
-  console.log(game);
 
   if (isLoading) return <Spinner />;
 
@@ -18,7 +14,7 @@ const GameDetailPage = () => {
   return (
     <>
       <Heading>{game.name}</Heading>
-      <Text>{game.description_raw}</Text>
+      <ExpandableText>{game.description_raw}</ExpandableText>
     </>
   );
 };
