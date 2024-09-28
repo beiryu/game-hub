388c932e08bf6b5acb04f6e6d0b113077af1f401
Beiryu <dinhnguyenkhanh210401@gmail.com>
2024-09-28 18:07:23 +0700

    refactor: entities interface

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -2,6 +2,7 @@ import { HStack, Image } from "@chakra-ui/react";
 import logo from "../../assets/logo.svg";
 import ColorModeSwitch from "../ColorModeSwitch";
 import SearchInput from "../SearchInput";
+import { Link } from "react-router-dom";
 
 interface Props {
   onSearch: (searchText: string) => void;
@@ -10,7 +11,9 @@ interface Props {
 const NavBar = ({ onSearch }: Props) => {
   return (
     <HStack justifyContent={"space-between"} padding="10px">
-      <Image src={logo} boxSize="60px" />
+      <Link to="/">
+        <Image src={logo} boxSize="60px" />
+      </Link>
       <SearchInput onSearch={onSearch} />
       <ColorModeSwitch />
     </HStack>
